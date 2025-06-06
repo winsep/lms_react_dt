179ad5b10f3c11fe0e4ea4004beeafef336ef4a9
winsep <winsep2004@gmail.com>
2025-06-07 04:15:56 +0700

    first commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,87 @@
+import { createContext, use, useEffect } from "react";
+import { dummyCourses } from "../assets/assets";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import humanizeDuration from "humanize-duration";
+
+export const AppContext = createContext()
+
+export const AppContextProvider = (props) => {
+
+    const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate()
+
+    const [allCourses, setAllCourses] = useState([]);
+    const [isEducator, setIsEducator] = useState(true)
+    const [enrolledCourses, setEnrolledCourses] = useState([])
+
+    // Fetch all courses from the server
+    const fetchAllCourses = async () => {
+        setAllCourses(dummyCourses)
+    }
+
+    // Function to calculate average rating of courses
+    const calculateRating = (course) => {
+        if(course.courseRatings.length === 0) {
+            return 0;
+        }
+        let totalRating = 0
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating
+        })
+        return totalRating / course.courseRatings.length
+    }
+
+    // Function to calculate Coourse chapter Time
+    const calculateChapterTime = (chapter) => {
+        let time = 0
+        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+        return humanizeDuration(time * 60 * 1000, {units:["h", "m"]})
+    }
+
+    // Function to Calculate Course Duration
+    const calculateCourseDuration = (course) => {
+        let time = 0
+
+        course.courseContent.map((chapter) => chapter.chapterContent.map(
+            (lecture)=> time += lecture.lectureDuration
+        ))
+        return humanizeDuration(time * 60 * 1000, {units:["h", "m"]})
+    }
+
+    // Function calculate to No of Lectures in a Course
+    const calculateNoOfLectures = (course) => {
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)) {
+                totalLectures += chapter.chapterContent.length;
+            }
+        });
+        return totalLectures;
+    }
+
+    // Fetch user's enrolled courses
+    const fetchEnrolledCourses = async () => {
+        setEnrolledCourses(dummyCourses)
+    }
+
+
+
+    useEffect(() => {
+        fetchAllCourses()
+        fetchEnrolledCourses()
+    }, [])
+
+    const value = {
+        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, 
+        calculateNoOfLectures, calculateCourseDuration, calculateChapterTime, enrolledCourses,
+        fetchEnrolledCourses
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+
+}
---
+++ client/src/pages/student/CoursesList.jsx
@@ -0,0 +1,60 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { AppContext } from '../../context/AppContext.jsx'
+import SearchBar from '../../components/student/SearchBar.jsx'
+import { useParams } from 'react-router-dom'
+import CourseCard from '../../components/student/CourseCard.jsx'
+import { assets } from '../../assets/assets.js'
+import Footer from '../../components/student/Footer.jsx'
+
+const CoursesList = () => {
+
+  const {navigate, allCourses} = useContext(AppContext)
+
+  const {input} = useParams()
+
+  const [filteredCouruse, setFilteredCouruse] = useState([])
+
+  useEffect(() => {
+    if(allCourses && allCourses.length > 0 ) {
+      const tempCourses = allCourses.slice()
+
+      input ?
+        setFilteredCouruse (
+          tempCourses.filter (
+            item => item.courseTitle.toLowerCase().includes(input.toLocaleLowerCase())
+          )
+        ) 
+      : setFilteredCouruse(tempCourses)
+    }
+  }, [allCourses, input])
+
+  return (
+    <>
+    <div className='relative md:px-36 px-8 pt-20 text-left'>
+      <div className='flex md:flex-row flex-col gap-6 items-start justify-between w-full'>
+        <div>
+          <h1 className='text-4xl font-semibold text-gray-800'>Course List</h1>
+        <p className='text-gray-500'>
+          <span className='text-blue-600 cursor-pointer' 
+          onClick={()=>navigate('/')}>Home</span> /<span> Course List</span></p>
+        </div>
+        <SearchBar data={input}/>
+      </div>
+
+      {
+        input && <div className='inline-flex items-center gap-4 px-4 py-2 border mt-8-mb-8 text-gray-600'>
+          <p>{input}</p>
+          <img src={assets.cross_icon} alt="" className='cursor-pointer' 
+          onClick={()=> navigate('/course-list')} />
+        </div>
+      }
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
+        {filteredCouruse.map((course, index) => <CourseCard key={index} course={course}/> )}
+      </div>
+    </div>
+    <Footer />
+    </>
+  )
+}
+
+export default CoursesList
