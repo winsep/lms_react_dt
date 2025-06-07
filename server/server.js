b144bd45d9adec4f74e5ab44a8e1e656e5eabf0f
winsep <winsep2004@gmail.com>
2025-06-07 23:18:39 +0700

    tris ngu

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
+import { clerkMiddleware } from '@clerk/express';
 import { clerkWebhooks } from './controllers/webhooks.js'
 
 // initialize express
@@ -12,6 +13,8 @@ connectDB()
 
 //middleware
 app.use(cors())
+app.use(express.json());
+app.use(clerkMiddleware());
 
 //routes
 app.get('/', (req, res) => res.send("API Working"))
