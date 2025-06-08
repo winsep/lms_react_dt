b829e2b7121024eeea99b6fc26d103d9dacd59c4
winsep <winsep2004@gmail.com>
2025-06-08 09:18:50 +0700

    c

--- server/models/User.js
+++ server/models/User.js
@@ -14,6 +14,6 @@ const userSchema = new mongoose.Schema(
         ],
     }, {timestamps: true }); 
 
-const User = mongoose.model('User',  userSchema);
+const User = mongoose.model('user',  userSchema);
 
 export default User
