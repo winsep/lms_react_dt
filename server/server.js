039f6a63fab44d930ff1ca7c9f4797212aa4e1a3
winsep <winsep2004@gmail.com>
2025-06-10 15:09:57 +0700

    stripe 2

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import educatorRouter from './routes/educatorRoutes.js';
 import connectCloudinary from './configs/cloudinary.js';
 import courseRouter from './routes/courseRoute.js';
 import userRouter from './routes/userRoutes.js';
+import bodyParser from 'body-parser'
 
 // initialize express
 const app = express()
@@ -27,7 +28,7 @@ app.use('/api/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
 app.use('/api/course', express.json(), courseRouter)
 app.use('/api/user', express.json(), userRouter)
-app.post('/stripe', express.json({type: 'application/json'}), stripeWebhooks)
+app.post('/stripe', bodyParser.raw({ type: 'application/json' }), stripeWebhooks)
 
 //port
 const PORT = process.env.PORT || 5000
