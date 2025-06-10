00f7c3f1261eda1f30eb1585bfc9efc326da2ff4
winsep <winsep2004@gmail.com>
2025-06-10 15:19:00 +0700

    stripe 3

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import bodyParser from 'body-parser'
+
 import connectDB from './configs/mongodb.js'
 import { clerkMiddleware } from '@clerk/express';
 import clerkWebhooks, { stripeWebhooks } from './controllers/webhooks.js'
@@ -8,7 +10,7 @@ import educatorRouter from './routes/educatorRoutes.js';
 import connectCloudinary from './configs/cloudinary.js';
 import courseRouter from './routes/courseRoute.js';
 import userRouter from './routes/userRoutes.js';
-import bodyParser from 'body-parser'
+
 
 // initialize express
 const app = express()
@@ -19,6 +21,9 @@ await connectCloudinary()
 
 //middleware
 app.use(cors())
+
+app.post('/stripe', bodyParser.raw({ type: 'application/json' }), stripeWebhooks)
+
 app.use(express.json());
 app.use(clerkMiddleware());
 
@@ -28,7 +33,7 @@ app.use('/api/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
 app.use('/api/course', express.json(), courseRouter)
 app.use('/api/user', express.json(), userRouter)
-app.post('/stripe', bodyParser.raw({ type: 'application/json' }), stripeWebhooks)
+
 
 //port
 const PORT = process.env.PORT || 5000
