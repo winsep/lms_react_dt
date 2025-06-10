2dee92b8548d271358ed5054779d30c24b780b81
winsep <winsep2004@gmail.com>
2025-06-10 16:07:15 +0700

    backend complete

--- server/models/Course.js
+++ server/models/Course.js
@@ -24,7 +24,7 @@ const courseSchema = new mongoose.Schema ({
     isPublished: { type: Boolean, default: true },
     discount: { type: Number, required: true, min: 0, max: 100 },
     courseContent: [chapterSchema],
-    courseRating: [
+    courseRatings: [
         {userId: {type: String}, rating: {type: Number, min: 1, max:5}}
     ],
     educator: {type: String, ref: 'user', required:true},
